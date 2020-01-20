0cccde5e0a1ea01571d36ab6272554f4720d5eeb
Yen-Bo Huang <yenbohuang@gmail.com>
2020-01-20 09:45:44 +0800

    add git scripts

---
+++ git/set-global-user.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+git config --global user.name "Yen-Bo Huang"
+git config --global user.email "yenbohuang@gmail.com"
---
+++ git/set-repo-user.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+git config user.name "Yen-Bo Huang"
+git config user.email "yenbohuang@gmail.com"
