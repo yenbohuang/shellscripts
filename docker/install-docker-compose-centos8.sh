2be2c9043a5cd35424cee53a3ad27dc8348e0bab
Yen-Bo Huang <yenbohuang@users.noreply.github.com>
2020-09-11 11:37:00 +0800

    Update install-docker-compose-centos8.sh

--- docker/install-docker-compose-centos8.sh
+++ docker/install-docker-compose-centos8.sh
@@ -2,5 +2,8 @@
 
 # Need python and pip
 sudo yum install python3
-
 pip3 install docker-compose
+
+# by direct download
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
