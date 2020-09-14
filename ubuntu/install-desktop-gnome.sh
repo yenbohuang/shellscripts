303567505ecc20e01c2bcea98e50d96b931c7997
Yen-Bo Huang <yenbohuang@users.noreply.github.com>
2020-09-14 11:21:31 +0800

    Update install-desktop-gnome.sh

--- ubuntu/install-desktop-gnome.sh
+++ ubuntu/install-desktop-gnome.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-https://linuxconfig.org/ubuntu-20-04-gnome-flashback-desktop-installation
+# https://linuxconfig.org/ubuntu-20-04-gnome-flashback-desktop-installation
 sudo apt install gnome-session-flashback
