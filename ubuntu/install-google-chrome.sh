e89fbac2da9b0c466975645fcd30c9c7b36e610a
Yen-Bo Huang <yenbohuang@users.noreply.github.com>
2020-09-15 09:28:05 +0800

    Create install-google-chrome.sh

---
+++ ubuntu/install-google-chrome.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-18-04/
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
