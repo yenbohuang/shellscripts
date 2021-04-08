23236787578acf27cc86070aae2eb639b67f5f3e
Yen-Bo Huang <yenbohuang@users.noreply.github.com>
2021-04-08 09:49:09 +0800

    Update gen-ssl-key.sh

--- gen-ssl-key.sh
+++ gen-ssl-key.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-# https://www.digicert.com/ssl-support/openssl-quick-reference-guide.htm
+# https://www.openssl.org/docs/man1.1.1/man1/openssl-req.html
+# https://www.openssl.org/docs/man1.1.1/man5/x509v3_config.html
 
-DOMAIN=$1
-DAYS=$2
+DOMAIN=localhost
+DAYS=365
 
-openssl req -new \
-  -newkey rsa:2048 -nodes -keyout $DOMAIN.key \
-  -out $DOMAIN.csr \
-  -subj "/C=TW/ST=Taiwan/L=Taipei/O=Yen-Bo Huang/OU=RD/CN=$DOMAIN"
+openssl req -x509 -newkey rsa:2048 -keyout $DOMAIN.key -out $DOMAIN.crt \
+  -sha256 -nodes -days $DAYS -config ./gen-ssl-key.conf
 
-openssl x509 -req -days $DAYS -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
-
-echo "*** verify CSR file ***"
-openssl req -text -in $DOMAIN.csr -noout -verify
-
-echo "*** verify CRT file ***"
-openssl x509 -text -in $DOMAIN.crt -noout
+openssl x509 -text -in $DOMAIN.crt -noout | less
